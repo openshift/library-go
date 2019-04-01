ad8422d41b7df3588539007c23424f3cca7f80e6
Michal Fojtik <mfojtik@redhat.com>
2019-04-01 16:31:25 +0200

    resourcesynccontroller: use cached get to confirm resource exists before deleting target

--- pkg/operator/resourcesynccontroller/resourcesync_controller.go
+++ pkg/operator/resourcesynccontroller/resourcesync_controller.go
@@ -149,6 +149,13 @@ func (c *ResourceSyncController) sync() error {
 
 	for destination, source := range c.configMapSyncRules {
 		if source == emptyResourceLocation {
+			// use the cache to check whether the configmap exists in target namespace, if not skip the extra delete call.
+			if _, err := c.configMapGetter.ConfigMaps(destination.Namespace).Get(destination.Name, metav1.GetOptions{}); err != nil {
+				if !apierrors.IsNotFound(err) {
+					errors = append(errors, err)
+				}
+				continue
+			}
 			if err := c.configMapGetter.ConfigMaps(destination.Namespace).Delete(destination.Name, nil); err != nil && !apierrors.IsNotFound(err) {
 				errors = append(errors, err)
 			}
@@ -162,6 +169,13 @@ func (c *ResourceSyncController) sync() error {
 	}
 	for destination, source := range c.secretSyncRules {
 		if source == emptyResourceLocation {
+			// use the cache to check whether the secret exists in target namespace, if not skip the extra delete call.
+			if _, err := c.secretGetter.Secrets(destination.Namespace).Get(destination.Name, metav1.GetOptions{}); err != nil {
+				if !apierrors.IsNotFound(err) {
+					errors = append(errors, err)
+				}
+				continue
+			}
 			if err := c.secretGetter.Secrets(destination.Namespace).Delete(destination.Name, nil); err != nil && !apierrors.IsNotFound(err) {
 				errors = append(errors, err)
 			}
