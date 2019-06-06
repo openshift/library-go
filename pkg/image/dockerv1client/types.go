e85e27029d4b49eee4307fde28f158dbc12bca19
Michal Fojtik <mfojtik@redhat.com>
2019-06-06 12:09:13 +0200

    image: add missing docker types

--- pkg/image/dockerv1client/types.go
+++ pkg/image/dockerv1client/types.go
@@ -1,5 +1,11 @@
 package dockerv1client
 
+import (
+	"time"
+
+	"github.com/openshift/api/image/docker10"
+)
+
 // TODO: Move these to openshift/api
 
 // DockerImageManifest represents the Docker v2 image format.
@@ -47,4 +53,39 @@ type Descriptor struct {
 	// Digest uniquely identifies the content. A byte stream can be verified
 	// against against this digest.
 	Digest string `json:"digest,omitempty"`
+}
+
+// DockerImageConfig stores the image configuration
+type DockerImageConfig struct {
+	ID              string                 `json:"id"`
+	Parent          string                 `json:"parent,omitempty"`
+	Comment         string                 `json:"comment,omitempty"`
+	Created         time.Time              `json:"created"`
+	Container       string                 `json:"container,omitempty"`
+	ContainerConfig docker10.DockerConfig  `json:"container_config,omitempty"`
+	DockerVersion   string                 `json:"docker_version,omitempty"`
+	Author          string                 `json:"author,omitempty"`
+	Config          *docker10.DockerConfig `json:"config,omitempty"`
+	Architecture    string                 `json:"architecture,omitempty"`
+	Size            int64                  `json:"size,omitempty"`
+	RootFS          *DockerConfigRootFS    `json:"rootfs,omitempty"`
+	History         []DockerConfigHistory  `json:"history,omitempty"`
+	OS              string                 `json:"os,omitempty"`
+	OSVersion       string                 `json:"os.version,omitempty"`
+	OSFeatures      []string               `json:"os.features,omitempty"`
+}
+
+// DockerConfigHistory stores build commands that were used to create an image
+type DockerConfigHistory struct {
+	Created    time.Time `json:"created"`
+	Author     string    `json:"author,omitempty"`
+	CreatedBy  string    `json:"created_by,omitempty"`
+	Comment    string    `json:"comment,omitempty"`
+	EmptyLayer bool      `json:"empty_layer,omitempty"`
+}
+
+// DockerConfigRootFS describes images root filesystem
+type DockerConfigRootFS struct {
+	Type    string   `json:"type"`
+	DiffIDs []string `json:"diff_ids,omitempty"`
 }
