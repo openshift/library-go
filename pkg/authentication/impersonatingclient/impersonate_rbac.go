6118e22fcc2f25451d08a9398e956c49e7b24152
Michal Fojtik <mfojtik@redhat.com>
2019-06-14 12:41:03 +0200

    auth: add impersonating client

---
+++ pkg/authentication/impersonatingclient/impersonate_rbac.go
@@ -0,0 +1,19 @@
+package impersonatingclient
+
+import (
+	"context"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apiserver/pkg/endpoints/request"
+	rbacv1 "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	"k8s.io/client-go/rest"
+)
+
+// NewImpersonatingRBACFromContext returns an impersonated RBAC client with the user of the context.
+func NewImpersonatingRBACFromContext(ctx context.Context, restclient rest.Interface) (rbacv1.RbacV1Interface, error) {
+	user, ok := request.UserFrom(ctx)
+	if !ok {
+		return nil, apierrors.NewBadRequest("user missing from context")
+	}
+	return rbacv1.New(NewImpersonatingRESTClient(user, restclient)), nil
+}
