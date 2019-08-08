a3f06133f0c1d4c244294d2b9a64c4a24b4a5ef1
Tomas Nozicka <tnozicka@gmail.com>
2019-08-08 10:54:10 +0200

    Symlink kubernetes staging in update-deps

--- alpha-build-machinery/make/targets/openshift/deps.mk
+++ alpha-build-machinery/make/targets/openshift/deps.mk
@@ -4,8 +4,22 @@ scripts_dir :=$(self_dir)/../../../scripts
 # We need to force localle so different envs sort files the same way for recursive traversals
 deps_diff :=LC_COLLATE=C diff --no-dereference -N
 
+define force-symlink
+	ln -sfn '$(1)' '$(2)'
+
+endef
+
+define k8s-symlink-staging
+	$(foreach \
+		repo, \
+		$(shell find ./vendor/k8s.io/kubernetes/staging/src/k8s.io/ -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | LC_COLLATE=C sort -h), \
+		$(call force-symlink,kubernetes/staging/src/k8s.io/$(repo),./vendor/k8s.io/$(repo)) \
+	)
+endef
+
 update-deps:
 	$(scripts_dir)/$@.sh
+	$(call k8s-symlink-staging)
 .PHONY: update-deps
 
 # $1 - temporary directory to restore vendor dependencies from glide.lock
@@ -15,6 +29,7 @@ define restore-deps
 	$(RM) -r "$(1)"/updated/vendor
 	cd "$(1)"/updated && glide install --strip-vendor && find ./vendor -name '.hg_archival.txt' -delete
 	cd "$(1)" && $(deps_diff) -r {current,updated}/vendor/ > updated/glide.diff || true
+	$(call k8s-symlink-staging)
 endef
 
 verify-deps: tmp_dir:=$(shell mktemp -d)
--- alpha-build-machinery/scripts/update-deps.sh
+++ alpha-build-machinery/scripts/update-deps.sh
@@ -1,16 +1,19 @@
-#!/bin/bash -e
+#!/bin/bash
+set -euo pipefail
+
+GLIDE=${GLIDE:-glide}
 
 readonly GLIDE_MINOR_VERSION="13"
 readonly REQUIRED_GLIDE_VERSION="0.$GLIDE_MINOR_VERSION"
 
 function verify_glide_version() {
-	if ! command -v glide &> /dev/null; then
+	if ! command -v ${GLIDE} &> /dev/null; then
 		echo "[FATAL] Glide was not found in \$PATH. Please install version ${REQUIRED_GLIDE_VERSION} or newer."
 		exit 1
 	fi
 
 	local glide_version
-	glide_version=($(glide --version))
+	glide_version=($(${GLIDE} --version))
 	if ! echo "${glide_version[2]#v}" | awk -F. -v min=$GLIDE_MINOR_VERSION '{ exit $2 < min }'; then
 		echo "Detected glide version: ${glide_version[*]}."
 		echo "Please install Glide version ${REQUIRED_GLIDE_VERSION} or newer."
@@ -20,7 +23,7 @@ function verify_glide_version() {
 
 verify_glide_version
 
-glide update --strip-vendor
+${GLIDE} update --strip-vendor
 
 # glide doesn't handle mercurial properly and leaves internal files (equivalent of .git/) laying around
 # Given those files differ by mercurial version it was cloned with, verify-deps would break
