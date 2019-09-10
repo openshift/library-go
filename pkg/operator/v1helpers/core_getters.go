3e1e6a3edbb5db4e5894bc0433282ca48900a2a9
Monis Khan <mkhan@redhat.com>
2019-09-10 15:30:01 -0400

    Handle get and list options in cached getters
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/operator/v1helpers/core_getters.go
+++ pkg/operator/v1helpers/core_getters.go
@@ -1,6 +1,8 @@
 package v1helpers
 
 import (
+	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -35,6 +37,9 @@ func (g combinedConfigMapGetter) ConfigMaps(namespace string) corev1client.Confi
 }
 
 func (g combinedConfigMapInterface) Get(name string, options metav1.GetOptions) (*corev1.ConfigMap, error) {
+	if err := validateResourceVersion(options.ResourceVersion); err != nil {
+		return nil, err
+	}
 	ret, err := g.lister.Get(name)
 	if err != nil {
 		return nil, err
@@ -42,14 +47,18 @@ func (g combinedConfigMapInterface) Get(name string, options metav1.GetOptions) 
 	return ret.DeepCopy(), nil
 }
 func (g combinedConfigMapInterface) List(opts metav1.ListOptions) (*corev1.ConfigMapList, error) {
-	list, err := g.lister.List(labels.Everything())
+	selector, err := listOptionsToSelector(opts)
+	if err != nil {
+		return nil, err
+	}
+	list, err := g.lister.List(selector)
 	if err != nil {
 		return nil, err
 	}
 
-	ret := &corev1.ConfigMapList{}
+	ret := &corev1.ConfigMapList{Items: make([]corev1.ConfigMap, len(list))}
 	for i := range list {
-		ret.Items = append(ret.Items, *(list[i].DeepCopy()))
+		ret.Items[i] = *(list[i].DeepCopy())
 	}
 	return ret, nil
 }
@@ -81,6 +90,9 @@ func (g combinedSecretGetter) Secrets(namespace string) corev1client.SecretInter
 }
 
 func (g combinedSecretInterface) Get(name string, options metav1.GetOptions) (*corev1.Secret, error) {
+	if err := validateResourceVersion(options.ResourceVersion); err != nil {
+		return nil, err
+	}
 	ret, err := g.lister.Get(name)
 	if err != nil {
 		return nil, err
@@ -89,14 +101,46 @@ func (g combinedSecretInterface) Get(name string, options metav1.GetOptions) (*c
 }
 
 func (g combinedSecretInterface) List(opts metav1.ListOptions) (*corev1.SecretList, error) {
-	list, err := g.lister.List(labels.Everything())
+	selector, err := listOptionsToSelector(opts)
+	if err != nil {
+		return nil, err
+	}
+	list, err := g.lister.List(selector)
 	if err != nil {
 		return nil, err
 	}
 
-	ret := &corev1.SecretList{}
+	ret := &corev1.SecretList{Items: make([]corev1.Secret, len(list))}
 	for i := range list {
-		ret.Items = append(ret.Items, *(list[i].DeepCopy()))
+		ret.Items[i] = *(list[i].DeepCopy())
 	}
 	return ret, nil
+}
+
+func listOptionsToSelector(options metav1.ListOptions) (labels.Selector, error) {
+	if err := validateResourceVersion(options.ResourceVersion); err != nil {
+		return nil, err
+	}
+
+	// make sure no unsupported fields are set (new fields to ListOptions are not silently ignored)
+	optionsCopy := *(options.DeepCopy())
+	optionsCopy.LabelSelector = ""           // setting label selector is supported and checked below
+	optionsCopy.TypeMeta = metav1.TypeMeta{} // ignore type meta
+	optionsCopy.TimeoutSeconds = nil         // ignore timeout
+	optionsCopy.Limit = 0                    // ignore limit
+	optionsCopy.ResourceVersion = ""         // we have already validated this above
+	if optionsEmpty := (metav1.ListOptions{}); optionsCopy != optionsEmpty {
+		return nil, fmt.Errorf("list options against cache has specified unsupported fields: %v", options)
+	}
+
+	return labels.Parse(options.LabelSelector)
+}
+
+func validateResourceVersion(resourceVersion string) error {
+	switch resourceVersion {
+	case "", "0":
+		return nil
+	default:
+		return fmt.Errorf("options against cache with specified resource version %q are unsupported", resourceVersion)
+	}
 }
