4bc2b3b93c5a70fef19f8d3e07f0e6c371f1b38d
Mike Dame <mdame@redhat.com>
2019-09-18 09:40:36 -0400

    Add second api version example

--- alpha-build-machinery/make/examples/multiple-binaries/pkg/apis/v1beta1/types.go
+++ alpha-build-machinery/make/examples/multiple-binaries/pkg/apis/v1beta1/types.go
@@ -1,4 +1,4 @@
-package v1
+package v1beta1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -8,18 +8,18 @@ import (
 // +genclient:nonNamespaced
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
-// +kubebuilder:storageversion
 
-// MyOperatorResource is an example operator configuration type
-type MyOperatorResource struct {
+// MyOtherOperatorResource is an example operator configuration type
+type MyOtherOperatorResource struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
 
 	// +kubebuilder:validation:Required
 	// +required
-	Spec MyOperatorResourceSpec `json:"spec"`
+	Spec MyOtherOperatorResourceSpec `json:"spec"`
 }
 
-type MyOperatorResourceSpec struct {
+type MyOtherOperatorResourceSpec struct {
 	Name string `json:"name"`
+	DeprecatedField string `json:"deprecatedField"`
 }
