aff91453e77de94746d0431c490f7a90f6e75c92
Michal Fojtik <mfojtik@redhat.com>
2019-11-28 10:32:53 +0100

    staticpod: rework controller initialization

--- pkg/operator/staticpod/controllers.go
+++ pkg/operator/staticpod/controllers.go
@@ -23,6 +23,11 @@ import (
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 )
 
+type RunnableController interface {
+	// TODO: Move this to ctx.Context
+	Run(workers int, stopCh <-chan struct{})
+}
+
 type staticPodOperatorControllerBuilder struct {
 	// clients and related
 	staticPodOperatorClient v1helpers.StaticPodOperatorClient
@@ -80,7 +85,7 @@ type Builder interface {
 	WithCerts(certDir string, certConfigMaps, certSecrets []revisioncontroller.RevisionResource) Builder
 	WithInstaller(command []string) Builder
 	WithPruning(command []string, staticPodPrefix string) Builder
-	ToControllers() (*staticPodOperatorControllers, error)
+	ToControllers() (RunnableController, error)
 }
 
 func (b *staticPodOperatorControllerBuilder) WithEvents(eventRecorder events.Recorder) Builder {
@@ -129,7 +134,7 @@ func (b *staticPodOperatorControllerBuilder) WithPruning(command []string, stati
 	return b
 }
 
-func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperatorControllers, error) {
+func (b *staticPodOperatorControllerBuilder) ToControllers() (RunnableController, error) {
 	controllers := &staticPodOperatorControllers{}
 
 	eventRecorder := b.eventRecorder
@@ -140,6 +145,7 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 	if versionRecorder == nil {
 		versionRecorder = status.NewVersionGetter()
 	}
+
 	configMapClient := v1helpers.CachedConfigMapGetter(b.kubeClient.CoreV1(), b.kubeInformers)
 	secretClient := v1helpers.CachedSecretGetter(b.kubeClient.CoreV1(), b.kubeInformers)
 	podClient := b.kubeClient.CoreV1()
@@ -147,8 +153,10 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 	operandInformers := b.kubeInformers.InformersFor(b.operandNamespace)
 	clusterInformers := b.kubeInformers.InformersFor("")
 
+	errs := []error{}
+
 	if len(b.operandNamespace) > 0 {
-		controllers.revisionController = revisioncontroller.NewRevisionController(
+		controllers.add(revisioncontroller.NewRevisionController(
 			b.operandNamespace,
 			b.revisionConfigMaps,
 			b.revisionSecrets,
@@ -157,11 +165,13 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 			configMapClient,
 			secretClient,
 			eventRecorder,
-		)
+		))
+	} else {
+		errs = append(errs, fmt.Errorf("missing revisionController; cannot proceed"))
 	}
 
 	if len(b.installCommand) > 0 {
-		controllers.installerController = installer.NewInstallerController(
+		controllers.add(installer.NewInstallerController(
 			b.operandNamespace,
 			b.staticPodName,
 			b.revisionConfigMaps,
@@ -177,20 +187,23 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 			b.certDir,
 			b.certConfigMaps,
 			b.certSecrets,
-		)
-		controllers.installerStateController = installerstate.NewInstallerStateController(
+		))
+
+		controllers.add(installerstate.NewInstallerStateController(
 			operandInformers,
 			podClient,
 			eventsClient,
 			b.staticPodOperatorClient,
 			b.operandNamespace,
 			eventRecorder,
-		)
+		))
+	} else {
+		errs = append(errs, fmt.Errorf("missing installerController; cannot proceed"))
 	}
 
 	if len(b.operandName) > 0 {
 		// TODO add handling for operator configmap changes to get version-mapping changes
-		controllers.staticPodStateController = staticpodstate.NewStaticPodStateController(
+		controllers.add(staticpodstate.NewStaticPodStateController(
 			b.operandNamespace,
 			b.staticPodName,
 			b.operatorNamespace,
@@ -201,11 +214,13 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 			podClient,
 			versionRecorder,
 			eventRecorder,
-		)
+		))
+	} else {
+		eventRecorder.Warning("StaticPodStateControllerMissing", "not enough information provided, not all functionality is present")
 	}
 
 	if len(b.pruneCommand) > 0 {
-		controllers.pruneController = prune.NewPruneController(
+		controllers.add(prune.NewPruneController(
 			b.operandNamespace,
 			b.staticPodPrefix,
 			b.pruneCommand,
@@ -214,25 +229,27 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 			podClient,
 			b.staticPodOperatorClient,
 			eventRecorder,
-		)
+		))
+	} else {
+		eventRecorder.Warning("PruningControllerMissing", "not enough information provided, not all functionality is present")
 	}
 
-	controllers.nodeController = node.NewNodeController(
+	controllers.add(node.NewNodeController(
 		b.staticPodOperatorClient,
 		clusterInformers,
 		eventRecorder,
-	)
+	))
 
-	controllers.backingResourceController = backingresource.NewBackingResourceController(
+	controllers.add(backingresource.NewBackingResourceController(
 		b.operandNamespace,
 		b.staticPodOperatorClient,
 		operandInformers,
 		b.kubeClient,
 		eventRecorder,
-	)
+	))
 
 	if b.dynamicClient != nil && b.enableServiceMonitorController {
-		controllers.monitoringResourceController = monitoring.NewMonitoringResourceController(
+		controllers.add(monitoring.NewMonitoringResourceController(
 			b.operandNamespace,
 			b.operandNamespace,
 			b.staticPodOperatorClient,
@@ -240,66 +257,26 @@ func (b *staticPodOperatorControllerBuilder) ToControllers() (*staticPodOperator
 			b.kubeClient,
 			b.dynamicClient,
 			eventRecorder,
-		)
+		))
 	}
 
-	controllers.unsupportedConfigOverridesController = unsupportedconfigoverridescontroller.NewUnsupportedConfigOverridesController(b.staticPodOperatorClient, eventRecorder)
-	controllers.logLevelController = loglevel.NewClusterOperatorLoggingController(b.staticPodOperatorClient, eventRecorder)
-
-	errs := []error{}
-	if controllers.revisionController == nil {
-		errs = append(errs, fmt.Errorf("missing revisionController; cannot proceed"))
-	}
-	if controllers.installerController == nil {
-		errs = append(errs, fmt.Errorf("missing installerController; cannot proceed"))
-	}
-	if controllers.installerStateController == nil {
-		errs = append(errs, fmt.Errorf("missing installerStateController; cannot proceed"))
-	}
-	if controllers.staticPodStateController == nil {
-		eventRecorder.Warning("StaticPodStateControllerMissing", "not enough information provided, not all functionality is present")
-	}
-	if controllers.pruneController == nil {
-		eventRecorder.Warning("PruningControllerMissing", "not enough information provided, not all functionality is present")
-	}
-	if controllers.monitoringResourceController == nil {
-		eventRecorder.Warning("MonitoringResourceController", "not enough information provided, not all functionality is present")
-	}
+	controllers.add(unsupportedconfigoverridescontroller.NewUnsupportedConfigOverridesController(b.staticPodOperatorClient, eventRecorder))
+	controllers.add(loglevel.NewClusterOperatorLoggingController(b.staticPodOperatorClient, eventRecorder))
 
 	return controllers, errors.NewAggregate(errs)
 }
 
 type staticPodOperatorControllers struct {
-	revisionController                   *revisioncontroller.RevisionController
-	installerController                  *installer.InstallerController
-	installerStateController             *installerstate.InstallerStateController
-	staticPodStateController             *staticpodstate.StaticPodStateController
-	pruneController                      *prune.PruneController
-	nodeController                       *node.NodeController
-	backingResourceController            *backingresource.BackingResourceController
-	monitoringResourceController         *monitoring.MonitoringResourceController
-	unsupportedConfigOverridesController *unsupportedconfigoverridescontroller.UnsupportedConfigOverridesController
-	logLevelController                   *loglevel.LogLevelController
+	controllers []RunnableController
 }
 
-func (o *staticPodOperatorControllers) WithInstallerPodMutationFn(installerPodMutationFn installer.InstallerPodMutationFunc) *staticPodOperatorControllers {
-	o.installerController.WithInstallerPodMutationFn(installerPodMutationFn)
-	return o
+func (o *staticPodOperatorControllers) add(controller RunnableController) {
+	o.controllers = append(o.controllers, controller)
 }
 
-func (o *staticPodOperatorControllers) Run(stopCh <-chan struct{}) {
-	go o.revisionController.Run(1, stopCh)
-	go o.installerController.Run(1, stopCh)
-	go o.installerStateController.Run(1, stopCh)
-	go o.staticPodStateController.Run(1, stopCh)
-	go o.pruneController.Run(1, stopCh)
-	go o.nodeController.Run(1, stopCh)
-	go o.backingResourceController.Run(1, stopCh)
-	go o.unsupportedConfigOverridesController.Run(1, stopCh)
-	go o.logLevelController.Run(1, stopCh)
-
-	if o.monitoringResourceController != nil {
-		go o.monitoringResourceController.Run(1, stopCh)
+func (o *staticPodOperatorControllers) Run(workers int, stopCh <-chan struct{}) {
+	for i := range o.controllers {
+		go o.controllers[i].Run(workers, stopCh)
 	}
 
 	<-stopCh
