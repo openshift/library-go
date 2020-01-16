7ee04522cd473cf704a3f75f2e670409ec652f59
Stanislav Laznicka <slaznick@redhat.com>
2020-01-16 14:45:04 +0100

    unify encryption controllers interface

--- pkg/operator/encryption/controllers.go
+++ pkg/operator/encryption/controllers.go
@@ -1,6 +1,8 @@
 package encryption
 
 import (
+	"context"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -33,7 +35,7 @@ func NewControllers(
 	secretsClient corev1.SecretsGetter,
 	eventRecorder events.Recorder,
 	encryptedGRs ...schema.GroupResource,
-) (*Controllers, error) {
+) *Controllers {
 	// avoid using the CachedSecretGetter as we need strong guarantees that our encryptionSecretSelector works
 	// otherwise we could see secrets from a different component (which will break our keyID invariants)
 	// this is fine in terms of performance since these controllers will be idle most of the time
@@ -94,17 +96,18 @@ func NewControllers(
 				encryptedGRs,
 			),
 		},
-	}, nil
+	}
 }
 
 type Controllers struct {
 	controllers []runner
 }
 
-func (c *Controllers) Run(stopCh <-chan struct{}) {
+// Run runs the encryption controllers, the number of workers is ignored
+func (c *Controllers) Run(ctx context.Context, _ int) {
 	for _, controller := range c.controllers {
 		con := controller // capture range variable
-		go con.Run(stopCh)
+		go con.Run(ctx.Done())
 	}
-	<-stopCh
+	<-ctx.Done()
 }
