7b9f983fc9366f51963331f4e775cd9e19ec0100
David Eads <deads@redhat.com>
2020-01-20 13:31:27 -0500

    add static resource controller to manage most resources

---
+++ pkg/operator/staticresourcecontroller/static_resource_controller.go
@@ -0,0 +1,211 @@
+package staticresourcecontroller
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	corev1 "k8s.io/api/core/v1"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
+
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+
+	"github.com/openshift/library-go/pkg/operator/resource/resourceapply"
+
+	"github.com/openshift/library-go/pkg/operator/events"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+)
+
+const (
+	workQueueKey = "key"
+)
+
+type StaticResourceController struct {
+	name      string
+	manifests resourceapply.AssetFunc
+	files     []string
+
+	operatorClient v1helpers.OperatorClient
+	clients        *resourceapply.ClientHolder
+
+	eventRecorder events.Recorder
+	// queue only ever has one item, but it has nice error handling backoff/retry semantics
+	queue workqueue.RateLimitingInterface
+}
+
+// NewStaticResourceController returns a controller that maintains certain static manifests. Most "normal" types are supported,
+// but feel free to add ones we missed.  Use .AddInformer() to provide triggering conditions.
+func NewStaticResourceController(
+	name string,
+	manifests resourceapply.AssetFunc,
+	files []string,
+	clients *resourceapply.ClientHolder,
+	operatorClient v1helpers.OperatorClient,
+	eventRecorder events.Recorder,
+) *StaticResourceController {
+	c := &StaticResourceController{
+		name:      name,
+		manifests: manifests,
+		files:     files,
+
+		operatorClient: operatorClient,
+		clients:        clients,
+
+		eventRecorder: eventRecorder.WithComponentSuffix(strings.ToLower(name)),
+		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), name),
+	}
+
+	c.operatorClient.Informer().AddEventHandler(c.eventHandler())
+
+	return c
+}
+
+func (c *StaticResourceController) AddEventHandler(addEventHandler func(handler cache.ResourceEventHandler)) *StaticResourceController {
+	addEventHandler(c.eventHandler())
+	return c
+}
+
+func (c *StaticResourceController) AddNamespaceEventHandler(addEventHandler func(handler cache.ResourceEventHandler), namespaces ...string) *StaticResourceController {
+	interestingNamespaces := sets.NewString(namespaces...)
+	addEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				ns, ok := obj.(*corev1.Namespace)
+				if !ok {
+					c.queue.Add(workQueueKey)
+				}
+				if interestingNamespaces.Has(ns.Name) {
+					c.queue.Add(workQueueKey)
+				}
+			},
+			UpdateFunc: func(old, new interface{}) {
+				ns, ok := old.(*corev1.Namespace)
+				if !ok {
+					c.queue.Add(workQueueKey)
+				}
+				if interestingNamespaces.Has(ns.Name) {
+					c.queue.Add(workQueueKey)
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				ns, ok := obj.(*corev1.Namespace)
+				if !ok {
+					tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+					if !ok {
+						utilruntime.HandleError(fmt.Errorf("couldn't get object from tombstone %#v", obj))
+						return
+					}
+					ns, ok = tombstone.Obj.(*corev1.Namespace)
+					if !ok {
+						utilruntime.HandleError(fmt.Errorf("tombstone contained object that is not a Namespace %#v", obj))
+						return
+					}
+				}
+				if interestingNamespaces.Has(ns.Name) {
+					c.queue.Add(workQueueKey)
+				}
+			},
+		})
+	return c
+}
+
+func (c StaticResourceController) sync() error {
+	errors := []error{}
+	directResourceResults := resourceapply.ApplyDirectly(c.clients, c.eventRecorder, c.manifests, c.files...)
+	for _, currResult := range directResourceResults {
+		if currResult.Error != nil {
+			errors = append(errors, fmt.Errorf("%q (%T): %v", currResult.File, currResult.Type, currResult.Error))
+			continue
+		}
+	}
+
+	if len(errors) > 0 {
+		message := ""
+		for _, err := range errors {
+			message = message + err.Error() + "\n"
+		}
+		errors = append(errors,
+			appendErrors(v1helpers.UpdateStatus(c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
+				Type:    fmt.Sprintf("%sDegraded", c.name),
+				Status:  operatorv1.ConditionTrue,
+				Reason:  "SyncError",
+				Message: message,
+			})))...,
+		)
+	} else {
+		errors = append(errors,
+			appendErrors(v1helpers.UpdateStatus(c.operatorClient, v1helpers.UpdateConditionFn(operatorv1.OperatorCondition{
+				Type:   fmt.Sprintf("%sDegraded", c.name),
+				Status: operatorv1.ConditionFalse,
+				Reason: "AsExpected",
+			})))...,
+		)
+	}
+
+	return utilerrors.NewAggregate(errors)
+}
+
+func appendErrors(_ *operatorv1.OperatorStatus, _ bool, err error) []error {
+	if err != nil {
+		return []error{err}
+	}
+	return []error{}
+}
+
+func (c *StaticResourceController) Run(ctx context.Context, workers int) {
+	defer utilruntime.HandleCrash()
+	defer c.queue.ShutDown()
+
+	klog.Infof("Starting %s", c.name)
+	defer klog.Infof("Shutting down %s", c.name)
+
+	// doesn't matter what workers say, only start one.
+	go wait.Until(c.runWorker, time.Second, ctx.Done())
+
+	// add time based trigger
+	go wait.Until(func() { c.queue.Add(workQueueKey) }, time.Minute, ctx.Done())
+
+	<-ctx.Done()
+}
+
+func (c *StaticResourceController) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+func (c *StaticResourceController) processNextWorkItem() bool {
+	dsKey, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	defer c.queue.Done(dsKey)
+
+	err := c.sync()
+	if err == nil {
+		c.queue.Forget(dsKey)
+		return true
+	}
+
+	utilruntime.HandleError(fmt.Errorf("%v failed with : %v", dsKey, err))
+	c.queue.AddRateLimited(dsKey)
+
+	return true
+}
+
+// eventHandler queues the operator to check spec and status
+func (c *StaticResourceController) eventHandler() cache.ResourceEventHandler {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) { c.queue.Add(workQueueKey) },
+		UpdateFunc: func(old, new interface{}) { c.queue.Add(workQueueKey) },
+		DeleteFunc: func(obj interface{}) { c.queue.Add(workQueueKey) },
+	}
+}
