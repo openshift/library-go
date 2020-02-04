ced4d81fc4822e200920332e2c8ec609d67d3fc3
Michal Fojtik <mfojtik@redhat.com>
2020-02-04 21:27:08 +0100

    build-machinery: use -trimpath by default

--- alpha-build-machinery/make/lib/golang.mk
+++ alpha-build-machinery/make/lib/golang.mk
@@ -32,7 +32,7 @@ endif
 GO_BUILD_PACKAGES ?=./cmd/...
 GO_BUILD_PACKAGES_EXPANDED ?=$(shell $(GO) list $(GO_MOD_FLAGS) $(GO_BUILD_PACKAGES))
 go_build_binaries =$(notdir $(GO_BUILD_PACKAGES_EXPANDED))
-GO_BUILD_FLAGS ?=
+GO_BUILD_FLAGS ?=-trimpath
 GO_BUILD_BINDIR ?=
 
 GO_TEST_FLAGS ?=-race
