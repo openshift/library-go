478f7ba99fa57fc19b6096591920324affea4986
Fabio Bertinatto <fabiojrb@gmail.com>
2020-03-17 14:16:34 +0100

    Add CSIDriver to resourceread and resourceapply
    The CSIDriver object contains information about a CSI driver.
    Up until recenty, this object was created by the cluster-driver-registrar
    sidecar container, however, this sidecar has been deprecated.
    As a result, operators need to create this object when installing a CSI driver.

--- pkg/operator/resource/resourceapply/storage.go
+++ pkg/operator/resource/resourceapply/storage.go
@@ -6,10 +6,12 @@ import (
 	"k8s.io/klog"
 
 	storagev1 "k8s.io/api/storage/v1"
+	storagev1beta1 "k8s.io/api/storage/v1beta1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	storageclientv1 "k8s.io/client-go/kubernetes/typed/storage/v1"
+	storageclientv1beta1 "k8s.io/client-go/kubernetes/typed/storage/v1beta1"
 
 	"github.com/openshift/library-go/pkg/operator/events"
 	"github.com/openshift/library-go/pkg/operator/resource/resourcemerge"
@@ -47,6 +49,35 @@ func ApplyStorageClass(client storageclientv1.StorageClassesGetter, recorder eve
 
 	// TODO if provisioner, parameters, reclaimpolicy, or volumebindingmode are different, update will fail so delete and recreate
 	actual, err := client.StorageClasses().Update(context.TODO(), existingCopy, metav1.UpdateOptions{})
+	reportUpdateEvent(recorder, required, err)
+	return actual, true, err
+}
+
+// ApplyCSIDriverV1Beta1 merges objectmeta, does not worry about anything else
+func ApplyCSIDriverV1Beta1(client storageclientv1beta1.CSIDriversGetter, recorder events.Recorder, required *storagev1beta1.CSIDriver) (*storagev1beta1.CSIDriver, bool, error) {
+	existing, err := client.CSIDrivers().Get(context.TODO(), required.Name, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		actual, err := client.CSIDrivers().Create(context.TODO(), required, metav1.CreateOptions{})
+		reportCreateEvent(recorder, required, err)
+		return actual, true, err
+	}
+	if err != nil {
+		return nil, false, err
+	}
+
+	modified := resourcemerge.BoolPtr(false)
+	existingCopy := existing.DeepCopy()
+
+	resourcemerge.EnsureObjectMeta(modified, &existingCopy.ObjectMeta, required.ObjectMeta)
+	if !*modified {
+		return existingCopy, false, nil
+	}
+
+	if klog.V(4) {
+		klog.Infof("CSIDriver %q changes: %v", required.Name, JSONPatchNoError(existing, existingCopy))
+	}
+
+	actual, err := client.CSIDrivers().Update(context.TODO(), existingCopy, metav1.UpdateOptions{})
 	reportUpdateEvent(recorder, required, err)
 	return actual, true, err
 }
