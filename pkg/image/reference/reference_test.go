openshift/library-go

commit 670e819f651ab346ab490c8bfd23e1298e2e49ac

image/reference: support parsing partial repo names
this is useful in mirroring contexts, where a set of images may be
mirrored into the same registry, or into the same namespace of a target

pkg/image/reference/reference.go
@@ -39,13 +39,25 @@ func Parse(spec string) (DockerImageReference, error) {
 
 	name := namedRef.Name()
 	i := strings.IndexRune(name, '/')
-	if i == -1 || (!strings.ContainsAny(name[:i], ":.") && name[:i] != "localhost") {
+
+	// if there are no path components, and it looks like a url (contains a .) or localhost, it's a registry
+	isRegistryOnly := i == -1 && (strings.ContainsAny(spec, ".") || strings.HasPrefix(spec, "localhost"))
+
+	// if there are no path components, and it's not a registry, it's a name
+	isNameOnly := i == -1 && !isRegistryOnly
+
+	// if there are path components, and the first component doesn't look like a url, it's a name
+	isNameOnly = isNameOnly || (i > -1 && (!strings.ContainsAny(name[:i], ":.") && name[:i] != "localhost"))
+
+	if isRegistryOnly {
+		ref.Registry = namedRef.String()
+	} else if isNameOnly {
 		ref.Name = name
 	} else {
 		ref.Registry, ref.Name = name[:i], name[i+1:]
 	}
 
-	if named, ok := namedRef.(reference.NamedTagged); ok {
+	if named, ok := namedRef.(reference.NamedTagged); !isRegistryOnly && ok {
 		ref.Tag = named.Tag()
 	}
 

pkg/image/reference/reference_test.go
@@ -163,6 +163,30 @@ func TestParse(t *testing.T) {
 			Tag:      "tag",
 		},
 		{
+			From:     "quay.io",
+			Registry: "quay.io",
+			Name:     "",
+			Tag:      "",
+		},
+		{
+			From:     "quay.io/org",
+			Registry: "quay.io",
+			Name:     "org",
+			Tag:      "",
+		},
+		{
+			From:     "localhost:5000",
+			Registry: "localhost:5000",
+			Name:     "",
+			Tag:      "",
+		},
+		{
+			From:     "localhost:5000/org",
+			Registry: "localhost:5000",
+			Name:     "org",
+			Tag:      "",
+		},
+		{
 			// registry/namespace/name > 255 chars
 			From: fmt.Sprintf("bar:5000/%s/%s:tag", strings.Repeat("a", 63), strings.Repeat("b", 183)),
 			Err:  true,

commit 76289f8feb2b3af2c3286cc456dcb17ec3135479

fix image reference parsing when tag includes a `.`
this was incorrectly detecting images without a registry, but with a
tag that included a '.' as a registry-only image reference, instead
of a name+tag image reference.

The corrects an issue introduced in 670e819

pkg/image/reference/reference.go
@@ -41,7 +41,7 @@ func Parse(spec string) (DockerImageReference, error) {
 	i := strings.IndexRune(name, '/')
 
 	// if there are no path components, and it looks like a url (contains a .) or localhost, it's a registry
-	isRegistryOnly := i == -1 && (strings.ContainsAny(spec, ".") || strings.HasPrefix(spec, "localhost"))
+	isRegistryOnly := i == -1 && (strings.ContainsAny(name, ".") || strings.HasPrefix(name, "localhost"))
 
 	// if there are no path components, and it's not a registry, it's a name
 	isNameOnly := i == -1 && !isRegistryOnly

pkg/image/reference/reference_test.go
@@ -187,6 +187,12 @@ func TestParse(t *testing.T) {
 			Tag:      "",
 		},
 		{
+			From:     "wildfly:15.0",
+			Registry: "",
+			Name:     "wildfly",
+			Tag:      "15.0",
+		},
+		{
 			// registry/namespace/name > 255 chars
 			From: fmt.Sprintf("bar:5000/%s/%s:tag", strings.Repeat("a", 63), strings.Repeat("b", 183)),
 			Err:  true,