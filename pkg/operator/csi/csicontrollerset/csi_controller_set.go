ffb186bf3657dcec12f1bdd076c523a33d8cccb4
Fabio Bertinatto <fbertina@redhat.com>
2020-12-09 10:51:10 -0300

    csi: add Controller Service (Deployment) hooks

--- pkg/operator/csi/csicontrollerset/csi_controller_set.go
+++ pkg/operator/csi/csicontrollerset/csi_controller_set.go
@@ -116,6 +116,7 @@ func (c *CSIControllerSet) WithCSIDriverControllerService(
 	kubeClient kubernetes.Interface,
 	namespacedInformerFactory informers.SharedInformerFactory,
 	optionalConfigInformer configinformers.SharedInformerFactory,
+	optionalDeploymentHooks ...csidrivercontrollerservicecontroller.DeploymentHookFunc,
 ) *CSIControllerSet {
 	manifestFile := assetFunc(file)
 	c.csiDriverControllerServiceController = csidrivercontrollerservicecontroller.NewCSIDriverControllerServiceController(
@@ -126,6 +127,7 @@ func (c *CSIControllerSet) WithCSIDriverControllerService(
 		namespacedInformerFactory.Apps().V1().Deployments(),
 		optionalConfigInformer,
 		c.eventRecorder,
+		optionalDeploymentHooks...,
 	)
 	return c
 }
