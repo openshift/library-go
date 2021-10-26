b59a3e8fc330083a86e3df7fa896ae89f07afb2d
Sam Batschelet <sbatsche@redhat.com>
2021-10-26 17:31:38 -0400

    pkg/operator/staticpod/controller/installer: remove dead code
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- pkg/operator/staticpod/controller/installer/installer_controller.go
+++ pkg/operator/staticpod/controller/installer/installer_controller.go
@@ -561,7 +561,6 @@ func setNodeStatusFn(status *operatorv1.NodeStatus) v1helpers.UpdateStaticPodSta
 func setAvailableProgressingNodeInstallerFailingConditions(newStatus *operatorv1.StaticPodOperatorStatus) error {
 	// Available means that we have at least one pod at the latest level
 	numAvailable := 0
-	numAtLatestRevision := 0
 	numProgressing := 0
 	counts := map[int32]int{}
 	failingCount := map[int32]int{}
@@ -580,9 +579,7 @@ func setAvailableProgressingNodeInstallerFailingConditions(newStatus *operatorv1
 			failing[currNodeStatus.LastFailedRevision] = append(failing[currNodeStatus.LastFailedRevision], currNodeStatus.LastFailedRevisionErrors...)
 		}
 
-		if newStatus.LatestAvailableRevision == currNodeStatus.CurrentRevision {
-			numAtLatestRevision += 1
-		} else {
+		if newStatus.LatestAvailableRevision != currNodeStatus.CurrentRevision {
 			numProgressing += 1
 		}
 	}
