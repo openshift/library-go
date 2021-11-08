5558388def01a58eb38e256b538993a6e2469d50
Michal Fojtik <mfojtik@redhat.com>
2021-11-08 16:08:17 +0100

    installerpod: skip installer pod UID generation when running on newer kubelets

--- pkg/operator/staticpod/installerpod/cmd_test.go
+++ pkg/operator/staticpod/installerpod/cmd_test.go
@@ -246,6 +246,19 @@ func TestCopyContent(t *testing.T) {
 	}
 }
 
+func TestKubeletVersion(t *testing.T) {
+	o := &InstallOptions{}
+	o.KubeletVersion = "1.22.1+1b2affc"
+	if o.installerPodNeedUUID() {
+		t.Fatalf("kubelet \"v1.22.1+1b2affc\" does not need UID")
+	}
+
+	o.KubeletVersion = "1.20.0+b12afff"
+	if !o.installerPodNeedUUID() {
+		t.Fatalf("kubelet \"v1.20.0+1b2affc\" need UID")
+	}
+}
+
 func checkFileContent(t *testing.T, file, expected string) {
 	actual, err := ioutil.ReadFile(file)
 	if err != nil {
