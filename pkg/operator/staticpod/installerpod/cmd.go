67ca3dec0baee15dd2f0863cd625b5a6d40ced72
David Eads <deads@redhat.com>
2021-12-02 14:11:56 -0500

    tighten static pod UID until v1.23 to account for kubelet bug fix

--- pkg/operator/staticpod/installerpod/cmd.go
+++ pkg/operator/staticpod/installerpod/cmd.go
@@ -445,7 +445,12 @@ func (o *InstallOptions) installerPodNeedUUID() bool {
 		return false
 	}
 	// if we run kubelet older than 4.9, installer pods require UID generation
-	return kubeletVersion.LT(semver.MustParse("1.22.0"))
+	// Switching this to 1.23 because there was a fix backported late to 1.22: https://github.com/kubernetes/kubernetes/pull/106394
+	// Once we prove this out in 1.23 after the kube bump, we can consider relaxing the constraint to 1.22.Z, but TRT
+	// suspects that this is failing installs due to
+	//  hyperkube[1371]: I1202 08:50:47.935427    1371 status_manager.go:611] "Pod was deleted and then recreated, skipping status update" pod="openshift-kube-scheduler/openshift-kube-scheduler-ip-10-0-129-204.us-east-2.compute.internal" oldPodUID=f11e314508a00e4ea9bf37d76b82b162 podUID=cce59ead72804895d31dba4208b395aa
+	// in kubelet logs
+	return kubeletVersion.LT(semver.MustParse("1.23.0"))
 }
 
 func (o *InstallOptions) writePod(rawPodBytes []byte, manifestFileName, resourceDir string) error {
