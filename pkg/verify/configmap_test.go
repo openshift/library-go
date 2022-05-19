4f76483c0e3c47a5d7c2257f45fb2d6561fc7b9b
W. Trevor King <wking@tremily.us>
2022-05-19 02:56:34 -0700

    pkg/verify: Test error handling for canceled sigstore HTTP calls
    Ensure that we have access to this data, so it's easy to see what's
    slow in situations like [1].
    
    [1]: https://bugzilla.redhat.com/show_bug.cgi?id=2071998#c2

--- pkg/verify/configmap_test.go
+++ pkg/verify/configmap_test.go
@@ -1,9 +1,15 @@
 package verify
 
 import (
+	"bytes"
+	"context"
+	"errors"
 	"io/ioutil"
+	"net/http"
 	"path/filepath"
+	"regexp"
 	"testing"
+	"time"
 
 	"golang.org/x/crypto/openpgp"
 
@@ -12,6 +18,38 @@ import (
 
 type VerifierAccessor interface {
 	Verifiers() map[string]openpgp.EntityList
+}
+
+// roundTripper implements http.RoundTripper in memory.
+type roundTripper struct {
+	data     map[string]string
+	delay    time.Duration
+	requests []string
+}
+
+// RoundTrip implements http.RoundTripper.
+func (rt *roundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
+	ctx := request.Context()
+	rt.requests = append(rt.requests, request.URL.String())
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case <-time.After(rt.delay):
+	}
+
+	data, ok := rt.data[request.URL.String()]
+	if !ok {
+		return &http.Response{
+			StatusCode: http.StatusNotFound,
+			Body:       ioutil.NopCloser(bytes.NewReader(nil)),
+		}, nil
+	}
+
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       ioutil.NopCloser(bytes.NewReader([]byte(data))),
+	}, nil
 }
 
 func Test_newFromConfigMapData(t *testing.T) {
@@ -76,5 +114,42 @@ func Test_newFromConfigMapData(t *testing.T) {
 				t.Fatalf("unexpected release verifier: %#v", got)
 			}
 		})
+	}
+}
+
+func Test_newFromConfigMapData_slow_sigstore(t *testing.T) {
+	redhatData, err := ioutil.ReadFile(filepath.Join("testdata", "keyrings", "redhat.txt"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rt := &roundTripper{
+		delay: time.Second,
+	}
+
+	verifier, err := newFromConfigMapData("from_test", map[string]string{
+		"store-example":              "https://example.com/signatures",
+		"verifier-public-key-redhat": string(redhatData),
+	}, func() (*http.Client, error) {
+		return &http.Client{Transport: rt}, nil
+	})
+
+	if err != nil {
+		t.Fatalf("newFromConfigMapData() error = %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+
+	err = verifier.Verify(ctx, "sha256:123")
+	expected := regexp.MustCompile(`^unable to verify sha256:123 against keyrings: verifier-public-key-redhat$`)
+	if !expected.MatchString(err.Error()) {
+		t.Fatalf("expected %s, got %s", expected, err.Error())
+	}
+
+	wrapped := errors.Unwrap(err)
+	expected = regexp.MustCompile(`^[[][0-9TZ:-]*: Get "https://example.com/signatures/sha256=123/signature-1": context deadline exceeded, [0-9TZ:-]*: context deadline exceeded]$`)
+	if !expected.MatchString(wrapped.Error()) {
+		t.Fatalf("expected %s, got %s", expected, wrapped.Error())
 	}
 }
