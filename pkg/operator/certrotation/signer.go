c1b3d395b296bf8fd0f4d77d2a88d4b06d844617
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-03-01 12:58:15 +0100

    certrotation: use shared metadata update code for signer and target secrets

--- pkg/operator/certrotation/signer.go
+++ pkg/operator/certrotation/signer.go
@@ -72,21 +72,9 @@ func (c RotatedSigningCASecret) EnsureSigningCertKeyPair(ctx context.Context) (*
 		}
 	}
 
-	needsMetadataUpdate := false
-	// no ownerReference set
-	if c.Owner != nil {
-		needsMetadataUpdate = ensureOwnerReference(&signingCertKeyPairSecret.ObjectMeta, c.Owner)
-	}
-	// ownership annotations not set
-	needsMetadataUpdate = c.AdditionalAnnotations.EnsureTLSMetadataUpdate(&signingCertKeyPairSecret.ObjectMeta) || needsMetadataUpdate
-	// convert outdated secret type (set pre 4.7)
-	if signingCertKeyPairSecret.Type != corev1.SecretTypeTLS {
-		signingCertKeyPairSecret.Type = corev1.SecretTypeTLS
-		needsMetadataUpdate = true
-	}
-	// apply changes (possibly via delete+recreate) if secret exists and requires metadata update
+	// apply necessary metadata (possibly via delete+recreate) if secret exists
 	// this is done before content update to prevent unexpected rollouts
-	if needsMetadataUpdate && len(signingCertKeyPairSecret.ResourceVersion) > 0 {
+	if ensureMetadataUpdate(signingCertKeyPairSecret, c.Owner, c.AdditionalAnnotations) && ensureSecretTLSTypeSet(signingCertKeyPairSecret) {
 		actualSigningCertKeyPairSecret, _, err := resourceapply.ApplySecret(ctx, c.Client, c.EventRecorder, signingCertKeyPairSecret)
 		if err != nil {
 			return nil, err
