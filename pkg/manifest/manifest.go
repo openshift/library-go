openshift/library-go

commit 679dc9ff647cb1becbb6ad2f8f274fde393bd4fd

React to changes coming from o/api features

pkg/manifest/manifest.go
@@ -26,15 +26,17 @@ const (
 	featureSetAnnotation  = "release.openshift.io/feature-set"
 )
 
-var knownFeatureSets = sets.String{}
+var knownFeatureSets = sets.Set[string]{}
 
 func init() {
-	for featureSet := range configv1.FeatureSets {
-		if len(featureSet) == 0 {
-			knownFeatureSets.Insert("Default")
-			continue
+	for _, featureSets := range configv1.AllFeatureSets() {
+		for featureSet := range featureSets {
+			if len(featureSet) == 0 {
+				knownFeatureSets.Insert("Default")
+				continue
+			}
+			knownFeatureSets.Insert(string(featureSet))
 		}
-		knownFeatureSets.Insert(string(featureSet))
 	}
 }
 
@@ -145,8 +147,8 @@ func (m *Manifest) populateFromObj() error {
 	return validateResourceId(m.id)
 }
 
-func getFeatureSets(annotations map[string]string) (sets.String, bool, error) {
-	ret := sets.String{}
+func getFeatureSets(annotations map[string]string) (sets.Set[string], bool, error) {
+	ret := sets.Set[string]{}
 	specified := false
 	for _, featureSetAnnotation := range []string{featureSetAnnotation} {
 		featureSetAnnotationValue, featureSetAnnotationExists := annotations[featureSetAnnotation]
@@ -156,7 +158,7 @@ func getFeatureSets(annotations map[string]string) (sets.String, bool, error) {
 			for _, manifestFeatureSet := range featureSetAnnotationValues {
 				if !knownFeatureSets.Has(manifestFeatureSet) {
 					// never include the manifest if the feature-set annotation is outside of known values
-					return nil, specified, fmt.Errorf("unrecognized value %q in %s=%s; known values are: %v", manifestFeatureSet, featureSetAnnotation, featureSetAnnotationValue, strings.Join(knownFeatureSets.List(), ","))
+					return nil, specified, fmt.Errorf("unrecognized value %q in %s=%s; known values are: %v", manifestFeatureSet, featureSetAnnotation, featureSetAnnotationValue, strings.Join(sets.List[string](knownFeatureSets), ","))
 				}
 			}
 			ret.Insert(featureSetAnnotationValues...)
@@ -176,7 +178,7 @@ func checkFeatureSets(requiredFeatureSet string, annotations map[string]string) 
 		return err
 	}
 	if manifestSpecifiesFeatureSets && !manifestFeatureSets.Has(requiredAnnotationValue) {
-		return fmt.Errorf("%q is required, and %s=%s", requiredAnnotationValue, featureSetAnnotation, strings.Join(manifestFeatureSets.List(), ","))
+		return fmt.Errorf("%q is required, and %s=%s", requiredAnnotationValue, featureSetAnnotation, strings.Join(sets.List[string](manifestFeatureSets), ","))
 	}
 
 	return nil

pkg/manifest/manifest_test.go
@@ -689,7 +689,7 @@ func Test_include(t *testing.T) {
 				"include.release.openshift.io/self-managed-high-availability": "true",
 				"release.openshift.io/feature-set":                            "Other",
 			},
-			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: CustomNoUpgrade,Default,LatencySensitive,TechPreviewNoUpgrade"),
+			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: Default,TechPreviewNoUpgrade"),
 		},
 		{
 			name:               "incorrect techpreview value is not excluded if techpreview on using feature-set",
@@ -699,7 +699,7 @@ func Test_include(t *testing.T) {
 				"include.release.openshift.io/self-managed-high-availability": "true",
 				"release.openshift.io/feature-set":                            "Other",
 			},
-			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: CustomNoUpgrade,Default,LatencySensitive,TechPreviewNoUpgrade"),
+			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: Default,TechPreviewNoUpgrade"),
 		},
 		{
 			name:        "default profile selection excludes without annotation",

pkg/features/features.go
@@ -17,8 +17,8 @@ type FeatureGateOptions struct {
 	featureGates    featuregate.FeatureGate
 }
 
-func NewFeatureGateOptions(featureGates featuregate.MutableFeatureGate, usedFeatures ...configv1.FeatureGateName) (*FeatureGateOptions, error) {
-	err := InitializeFeatureGates(featureGates, usedFeatures...)
+func NewFeatureGateOptions(featureGates featuregate.MutableFeatureGate, profileName configv1.ClusterProfileName, usedFeatures ...configv1.FeatureGateName) (*FeatureGateOptions, error) {
+	err := InitializeFeatureGates(featureGates, profileName, usedFeatures...)
 	if err != nil {
 		return nil, err
 	}
@@ -28,8 +28,8 @@ func NewFeatureGateOptions(featureGates featuregate.MutableFeatureGate, usedFeat
 	}, nil
 }
 
-func NewFeatureGateOptionsOrDie(featureGates featuregate.MutableFeatureGate, usedFeatures ...configv1.FeatureGateName) *FeatureGateOptions {
-	ret, err := NewFeatureGateOptions(featureGates, usedFeatures...)
+func NewFeatureGateOptionsOrDie(featureGates featuregate.MutableFeatureGate, profileName configv1.ClusterProfileName, usedFeatures ...configv1.FeatureGateName) *FeatureGateOptions {
+	ret, err := NewFeatureGateOptions(featureGates, profileName, usedFeatures...)
 	if err != nil {
 		panic(err)
 	}
@@ -87,14 +87,15 @@ func setFeatureGates(featureGatesMap map[string]bool, featureGates featuregate.M
 
 // InitializeFeatureGates should be called when your binary is starting with your featuregate instance and the list of
 // featuregates that your process will honor.
-func InitializeFeatureGates(featureGates featuregate.MutableFeatureGate, usedFeatures ...configv1.FeatureGateName) error {
-	defaultFeatures := sets.String{}
-	enabledDefaultFeatures := sets.String{}
-	for _, enabled := range configv1.FeatureSets[configv1.Default].Enabled {
+func InitializeFeatureGates(featureGates featuregate.MutableFeatureGate, profileName configv1.ClusterProfileName, usedFeatures ...configv1.FeatureGateName) error {
+	defaultFeatures := sets.Set[string]{}
+	enabledDefaultFeatures := sets.Set[string]{}
+	allFeatureSets := configv1.AllFeatureSets()[profileName]
+	for _, enabled := range allFeatureSets[configv1.Default].Enabled {
 		defaultFeatures.Insert(string(enabled.FeatureGateAttributes.Name))
 		enabledDefaultFeatures.Insert(string(enabled.FeatureGateAttributes.Name))
 	}
-	for _, disabled := range configv1.FeatureSets[configv1.Default].Disabled {
+	for _, disabled := range allFeatureSets[configv1.Default].Disabled {
 		defaultFeatures.Insert(string(disabled.FeatureGateAttributes.Name))
 	}
 

commit a2c1a9336667c942a2bbebdbd54e24d9691431b8

pkg/manifest: Restore CustomNoUpgrade handling
Avoiding failures like:

  $ curl -s https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/openshift_cluster-capi-operator/181/pull-ci-openshift-cluster-capi-operator-main-e2e-aws-capi-techpreview/1806459134145466368/artifacts/e2e-aws-capi-techpreview/gather-extra/artifacts/pods/openshift-cluster-version_cluster-version-operator-85cdd86f99-nrqd6_cluster-version-operator_previous.log | grep '0000_30_cluster-api_00_namespace.yaml'
  I0627 23:22:20.849886       1 payload.go:210] excluding Filename: "0000_30_cluster-api_00_namespace.yaml" Group: "" Kind: "Namespace" Name: "openshift-cluster-api": unrecognized value "CustomNoUpgrade" in release.openshift.io/feature-set=CustomNoUpgrade,TechPreviewNoUpgrade; known values are: Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade
  I0627 23:22:21.820724       1 payload.go:210] excluding Filename: "0000_30_cluster-api_00_namespace.yaml" Group: "" Kind: "Namespace" Name: "openshift-cluster-api": unrecognized value "CustomNoUpgrade" in release.openshift.io/feature-set=CustomNoUpgrade,TechPreviewNoUpgrade; known values are: Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade

because 679dc9ff64 (React to changes coming from o/api features,
2024-04-05, #1711) accidentally dropped the set.

679dc9ff64 also removed LatencySensitive, although that is ok because
[1] intentionally pushed LatencySensitive clusters back into the
default feature set in 4.14.

[1]: http://issues.redhat.com/browse/OCPBUGS-15877

pkg/manifest/manifest.go
@@ -40,6 +40,7 @@ func init() {
 			knownFeatureSets.Insert(string(featureSet))
 		}
 	}
+	knownFeatureSets.Insert(string(configv1.CustomNoUpgrade))
 }
 
 // resourceId uniquely identifies a Kubernetes resource.

pkg/manifest/manifest_test.go
@@ -688,7 +688,7 @@ func Test_include(t *testing.T) {
 				"include.release.openshift.io/self-managed-high-availability": "true",
 				"release.openshift.io/feature-set":                            "Other",
 			},
-			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade"),
+			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: CustomNoUpgrade,Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade"),
 		},
 		{
 			name:               "incorrect techpreview value is not excluded if techpreview on using feature-set",
@@ -698,7 +698,7 @@ func Test_include(t *testing.T) {
 				"include.release.openshift.io/self-managed-high-availability": "true",
 				"release.openshift.io/feature-set":                            "Other",
 			},
-			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade"),
+			expected: fmt.Errorf("unrecognized value \"Other\" in release.openshift.io/feature-set=Other; known values are: CustomNoUpgrade,Default,DevPreviewNoUpgrade,TechPreviewNoUpgrade"),
 		},
 		{
 			name:        "default profile selection excludes without annotation",