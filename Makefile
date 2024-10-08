45003c53c61598f193329cd04eb2cd4212840b2b
Pranshu Srivastava <rexagod@gmail.com>
2024-10-09 00:35:56 +0530

    Set resource version for CRs
    Set resource version for all `monitoring.go` GVRs. Earlier, this was not
    done by the machinery responsible for it, which caused manifest
    applications that had no resource version present to encounter the
    following error:
    ```
    metadata.resourceVersion: Invalid value: 0x0: must be specified for an update
    ```
    This patch addresses that regression, which was introduced originally in #1575.
    
    Signed-off-by: Pranshu Srivastava <rexagod@gmail.com>

--- Makefile
+++ Makefile
@@ -26,5 +26,5 @@ verify-podnetworkconnectivitychecks:
 test-e2e-encryption: GO_TEST_PACKAGES :=./test/e2e-encryption/...
 .PHONY: test-e2e-encryption
 
-test-e2e-monitoring:
+test-e2e-monitoring: GO_TEST_PACKAGES :=./test/e2e-monitoring/...
 .PHONY: test-e2e-monitoring
--- pkg/operator/resource/resourceapply/monitoring_test.go
+++ pkg/operator/resource/resourceapply/monitoring_test.go
@@ -2,14 +2,16 @@ package resourceapply
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/util/diff"
 	"k8s.io/apimachinery/pkg/util/json"
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 	clienttesting "k8s.io/client-go/testing"
@@ -19,31 +21,48 @@ import (
 	"github.com/openshift/library-go/pkg/operator/events"
 )
 
+var structuredServiceMonitor = pov1api.ServiceMonitor{
+	TypeMeta: metav1.TypeMeta{
+		APIVersion: "monitoring.coreos.com/v1",
+		Kind:       "ServiceMonitor",
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "test-sm",
+		Namespace: "test-ns",
+		Labels:    map[string]string{"app": "test-app"},
+	},
+	Spec: pov1api.ServiceMonitorSpec{
+		NamespaceSelector: pov1api.NamespaceSelector{
+			MatchNames: []string{"test-ns"},
+		},
+	},
+}
+
 func TestApplyServiceMonitor(t *testing.T) {
 	dynamicScheme := runtime.NewScheme()
 	dynamicScheme.AddKnownTypeWithName(schema.GroupVersionKind{Group: "monitoring.coreos.com", Version: "v1", Kind: "ServiceMonitor"}, &unstructured.Unstructured{})
 
-	structuredServiceMonitor := pov1api.ServiceMonitor{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "monitoring.coreos.com/v1",
-			Kind:       "ServiceMonitor",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-sm",
-			Namespace: "test-ns",
-			Labels:    map[string]string{"app": "test-app"},
-		},
-		Spec: pov1api.ServiceMonitorSpec{
-			NamespaceSelector: pov1api.NamespaceSelector{
-				MatchNames: []string{"test-ns"},
-			},
-		},
-	}
 	unstructuredServiceMonitor := structuredToUnstructuredServiceMonitor(&structuredServiceMonitor)
 
 	structuredServiceMonitorDifferentLabels := structuredServiceMonitor.DeepCopy()
-	structuredServiceMonitorDifferentLabels.Labels = map[string]string{"app": "different-test-app"}
+	structuredServiceMonitorDifferentLabels.Labels = map[string]string{"fake-app": "fake-test-app"}
 	unstructuredServiceMonitorDifferentLabels := structuredToUnstructuredServiceMonitor(structuredServiceMonitorDifferentLabels)
+	serviceMonitorDifferentLabelsValidationFunc := func(oI interface{}) error {
+		o, ok := oI.(*unstructured.Unstructured)
+		if !ok {
+			return errors.New("unexpected object type")
+		}
+		gotLabels := o.GetLabels()
+		wantLabels := structuredServiceMonitor.Labels
+		for k, v := range structuredServiceMonitorDifferentLabels.Labels {
+			wantLabels[k] = v
+		}
+		if !equality.Semantic.DeepEqual(gotLabels, wantLabels) {
+			return errors.New(fmt.Sprintf("service monitor labels were not merged correctly (+got, -want): %s", cmp.Diff(gotLabels, wantLabels)))
+		}
+
+		return nil
+	}
 
 	structuredServiceMonitorDifferentSpec := structuredServiceMonitor.DeepCopy()
 	structuredServiceMonitorDifferentSpec.Spec.NamespaceSelector.MatchNames = []string{"different-test-ns"}
@@ -58,6 +77,7 @@ func TestApplyServiceMonitor(t *testing.T) {
 		existing                           *unstructured.Unstructured
 		expectExistingResourceToBeModified bool
 		expectActionsDuringModification    []string
+		delegateValidation                 func(interface{}) error
 	}{
 		{
 			name:                            "same label, same spec",
@@ -69,6 +89,7 @@ func TestApplyServiceMonitor(t *testing.T) {
 			existing:                           unstructuredServiceMonitorDifferentLabels,
 			expectExistingResourceToBeModified: true,
 			expectActionsDuringModification:    []string{"get", "update"},
+			delegateValidation:                 serviceMonitorDifferentLabelsValidationFunc,
 		},
 		{
 			name:                               "same label, different spec",
@@ -85,7 +106,7 @@ func TestApplyServiceMonitor(t *testing.T) {
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			dynamicClient := dynamicfake.NewSimpleDynamicClient(dynamicScheme, tc.existing)
-			_, modified, err := ApplyServiceMonitor(context.TODO(), dynamicClient, events.NewInMemoryRecorder("monitor-test"), unstructuredServiceMonitor)
+			got, modified, err := ApplyServiceMonitor(context.TODO(), dynamicClient, events.NewInMemoryRecorder("monitor-test"), unstructuredServiceMonitor)
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
@@ -103,7 +124,11 @@ func TestApplyServiceMonitor(t *testing.T) {
 				}
 			}
 
-			if len(tc.expectActionsDuringModification) > 1 &&
+			if tc.delegateValidation != nil {
+				if err = tc.delegateValidation(got); err != nil {
+					t.Fatalf("delegated validation failed: %v", err)
+				}
+			} else if len(tc.expectActionsDuringModification) > 1 &&
 				tc.expectActionsDuringModification[1] == "update" {
 				updateAction, isUpdate := actions[1].(clienttesting.UpdateAction)
 				if !isUpdate {
@@ -111,18 +136,18 @@ func TestApplyServiceMonitor(t *testing.T) {
 				}
 				updatedMonitorObj := updateAction.GetObject().(*unstructured.Unstructured)
 
-				// Verify `metadata`.
+				// Verify `metadata`. Note that the `metadata` is merged in some cases.
 				requiredMonitorMetadata, _, _ := unstructured.NestedMap(unstructuredServiceMonitor.UnstructuredContent(), "metadata")
 				existingMonitorMetadata, _, _ := unstructured.NestedMap(updatedMonitorObj.UnstructuredContent(), "metadata")
 				if !equality.Semantic.DeepEqual(requiredMonitorMetadata, existingMonitorMetadata) {
-					t.Fatalf("expected resulting service monitor metadata to match required metadata: %s", diff.ObjectDiff(requiredMonitorMetadata, existingMonitorMetadata))
+					t.Fatalf("expected resulting service monitor metadata to match required metadata: %s", cmp.Diff(requiredMonitorMetadata, existingMonitorMetadata))
 				}
 
 				// Verify `spec`.
 				requiredMonitorSpec, _, _ := unstructured.NestedMap(unstructuredServiceMonitor.UnstructuredContent(), "spec")
 				existingMonitorSpec, _, _ := unstructured.NestedMap(updatedMonitorObj.UnstructuredContent(), "spec")
 				if !equality.Semantic.DeepEqual(requiredMonitorSpec, existingMonitorSpec) {
-					t.Fatalf("expected resulting service monitor spec to match required spec: %s", diff.ObjectDiff(requiredMonitorMetadata, existingMonitorMetadata))
+					t.Fatalf("expected resulting service monitor spec to match required spec: %s", cmp.Diff(requiredMonitorSpec, existingMonitorSpec))
 				}
 			}
 		})
