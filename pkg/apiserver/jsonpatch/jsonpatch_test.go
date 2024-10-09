75f07ed0babcd928782fcd42082be31d76516b6e
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2024-10-09 12:04:52 +0200

    intro json patch library

---
+++ pkg/apiserver/jsonpatch/jsonpatch_test.go
@@ -0,0 +1,55 @@
+package jsonpatch
+
+import (
+	"testing"
+)
+
+func TestJSONPatch(t *testing.T) {
+	scenarios := []struct {
+		name           string
+		target         *PatchSet
+		expectedOutput string
+	}{
+		{
+			name:           "empty patch works and encodes as the null JSON value",
+			target:         New(),
+			expectedOutput: "null",
+		},
+		{
+			name:           "patch WithTest",
+			target:         New().WithTest("/metadata/resourceVersion", "1234"),
+			expectedOutput: `[{"op":"test","path":"/metadata/resourceVersion","value":"1234"}]`,
+		},
+		{
+			name:           "patch WithTest and WithRemove",
+			target:         New().WithRemove("/status/foo", NewTestCondition("/status/condition", "bar")),
+			expectedOutput: `[{"op":"test","path":"/status/condition","value":"bar"},{"op":"remove","path":"/status/foo"}]`,
+		},
+		{
+			name:           "patch WithTest and WithRemove multiple times same test",
+			target:         New().WithRemove("/status/foo", NewTestCondition("/status/condition", "bar")).WithRemove("/status/bar", NewTestCondition("/status/condition", "bar")),
+			expectedOutput: `[{"op":"test","path":"/status/condition","value":"bar"},{"op":"test","path":"/status/condition","value":"bar"},{"op":"remove","path":"/status/foo"},{"op":"remove","path":"/status/bar"}]`,
+		},
+		{
+			name:           "patch WithTest and WithRemove multiple times different test",
+			target:         New().WithRemove("/status/foo", NewTestCondition("/status/condition", "bar")).WithRemove("/status/bar", NewTestCondition("/status/condition", "foo")),
+			expectedOutput: `[{"op":"test","path":"/status/condition","value":"foo"},{"op":"test","path":"/status/condition","value":"bar"},{"op":"remove","path":"/status/foo"},{"op":"remove","path":"/status/bar"}]`,
+		},
+		{
+			name:           "patch WithTest multiple times",
+			target:         New().WithTest("/status/secondCondition", "foo").WithRemove("/status/foo", NewTestCondition("/status/condition", "bar")),
+			expectedOutput: `[{"op":"test","path":"/status/condition","value":"bar"},{"op":"test","path":"/status/secondCondition","value":"foo"},{"op":"remove","path":"/status/foo"}]`,
+		},
+	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			patchBytes, err := scenario.target.Marshal()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(patchBytes) != scenario.expectedOutput {
+				t.Fatalf("expected = %s, got = %s", scenario.expectedOutput, patchBytes)
+			}
+		})
+	}
+}
