d9b942c2bac2831905266fe7ad46a4de67c230e9
Damien Grisonnet <dgrisonn@redhat.com>
2025-07-23 14:44:25 +0200

    revision_controller: fix revision readiness
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- pkg/operator/revisioncontroller/revision_controller.go
+++ pkg/operator/revisioncontroller/revision_controller.go
@@ -366,6 +366,21 @@ func (c RevisionController) sync(ctx context.Context, syncCtx factory.SyncContex
 		return nil
 	}
 
+	// Make sure that revision-ready is set to true on the
+	// revision-status{status.latestAvailableRevision} configmap. This prevents
+	// blocking the controller when upgrading to a version that checks revision
+	// readiness.
+	configMap, err := c.configMapGetter.ConfigMaps(c.targetNamespace).Get(ctx, nameFor("revision-status", operatorStatus.LatestAvailableRevision), metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	if configMap.Annotations["operator.openshift.io/revision-ready"] != "true" {
+		configMap.Annotations["operator.openshift.io/revision-ready"] = "true"
+		if _, err := c.configMapGetter.ConfigMaps(configMap.Namespace).Update(ctx, configMap, metav1.UpdateOptions{}); err != nil {
+			return err
+		}
+	}
+
 	// If the operator status's latest available revision is not the same as the observed latest revision, update the operator.
 	// This needs to be done even if the revision precondition is not required because it ensures our operator status is
 	// correct for all consumers.
